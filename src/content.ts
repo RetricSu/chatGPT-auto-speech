d6171bdc6b13b97f7b436528b91e7abb82de3c47
RetricSu <inneverland2013@gmail.com>
2022-12-09 00:29:13 +0800

    fix: speak with english or chinese detection

--- src/content.ts
+++ src/content.ts
@@ -50,9 +50,7 @@ window.addEventListener("load", (_event: Event) => {
     }
 
     // Create a new SpeechSynthesisUtterance with the text
-    const synth = window.speechSynthesis;
-    const utterance = new SpeechSynthesisUtterance(text);
-    synth.speak(utterance);
+    speakLang(text);
 
     // Increment the counter variable to move to the next position in the Map
     currentIndex++;
@@ -99,3 +97,75 @@ window.addEventListener("load", (_event: Event) => {
   // Use the setInterval() method to execute the clickButton() function every 5 seconds
   setInterval(clickButton, 5000);
 });
+
+// utils functions
+// below is all wrote by chatGPT
+enum Language {
+  Chinese = "chinese",
+  English = "english",
+  Unknown = "unknown",
+}
+
+function isEnglishOrChinese(texts: string): Language {
+  // Use a regular expression to match Chinese characters
+  const chineseRegex = /[\u4e00-\u9fff]/;
+
+  // Use a regular expression to match English letters
+  const englishRegex = /^[A-Za-z]+$/;
+
+  // Count the number of Chinese characters and English letters
+  // in the text
+  let chineseCount = 0;
+  let englishCount = 0;
+  for (let i = 0; i < texts.length; i++) {
+    if (chineseRegex.test(texts[i])) {
+      chineseCount++;
+    } else if (englishRegex.test(texts[i])) {
+      englishCount++;
+    }
+  }
+
+  // Check if the text contains more Chinese characters or
+  // English letters
+  if (chineseCount > englishCount) {
+    // The text is written in Chinese
+    return Language.Chinese;
+  } else if (englishCount > chineseCount) {
+    // The text is written in English
+    return Language.English;
+  } else {
+    // The text contains an equal number of Chinese
+    // characters and English letters
+    return Language.Unknown;
+  }
+}
+
+function speakLang(texts: string) {
+  // Use the isEnglishOrChinese() function to determine the
+  // dominant language of the text
+  const language = isEnglishOrChinese(texts);
+
+  // Create a new SpeechSynthesisUtterance object
+  let utterance = new SpeechSynthesisUtterance();
+  const speechSynthesis = window.speechSynthesis;
+
+  // Set the text and language of the utterance based on the
+  // dominant language of the text
+  if (language === Language.Chinese) {
+    utterance.text = texts;
+    utterance.lang = "zh-CN";
+    // Speak the text using the SpeechSynthesisUtterance API
+    speechSynthesis.speak(utterance);
+  } else if (language === Language.English) {
+    utterance.text = texts;
+    utterance.lang = "en-US";
+    // Speak the text using the SpeechSynthesisUtterance API
+    speechSynthesis.speak(utterance);
+  } else {
+    // The text is written in a mix of Chinese and English,
+    // so we can't determine the dominant language
+    console.log(
+      "Unable to determine the dominant language of the text, can't speak"
+    );
+  }
+}
