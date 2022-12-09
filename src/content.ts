0026a2ca828866e054689e7fb35430fc14e54c55
RetricSu <inneverland2013@gmail.com>
2022-12-09 12:25:51 +0800

    feat: speak simultaneously

--- src/content.ts
+++ src/content.ts
@@ -1,102 +1,91 @@
+// Create a counter variable to keep track of the position of the currently playing text
+let currentIndex = 0;
+
 // Listen for the "load" event on the window object
-window.addEventListener("load", (_event: Event) => {
-  // Inject the "Listen" button into the page using the Chrome Extension API
-  // Create the "Listen" button
-  // TypeScript
+window.addEventListener("load", async (_event: Event) => {
+  findResponseTextToSpeak();
+});
 
-  const button = document.createElement("button") as HTMLButtonElement;
-  button.id = "listen-button";
-  button.textContent = "Listen";
+// find and get response text to speak
+async function findResponseTextToSpeak() {
+  // Select all elements that you want to play
+  const elements = document.querySelectorAll(
+    ".bg-gray-50 > .text-base"
+  ) as NodeListOf<HTMLElement>;
+  console.log("find elements =>", elements.length);
 
-  // Set the width and height of the button to 100 pixels
-  button.style.width = "80px";
-  button.style.height = "20px";
+  // Add the text from each element to the Map
+  await elements.forEach(async (element, index) => {
+    if (
+      index >= currentIndex &&
+      element.innerText != null &&
+      element.innerText.length > 3
+    ) {
+      // only add the newer element into the map
+      // only read text inside P elements, to exclude reading code
+      const text = findParagraphTexts(element);
+      console.log("find new text => ", text);
 
-  // Set the background color of the button to blue
-  button.style.backgroundColor = "blue";
-  button.style.opacity = "0.5";
+      // turn the text into speech
+      await speakLang(text);
+      // keep watching if text has been added new words
+      await watchParagraphTextsToContinueSpeak(
+        element,
+        element.innerText.length,
+        text
+      );
 
-  // Position the button in the center of the page using the left and top properties
-  button.style.position = "fixed";
-  button.style.left = "50%";
-  button.style.top = "50%";
-  button.style.display = "none";
-
-  // Add an animation to the button when it is clicked
-  button.style.animation = "fade 0.5s";
-
-  // Add the button to the page using the document.add method
-  document.body.appendChild(button);
-
-  // Create a Map to save the text from each element
-  const textMap = new Map<number, string>();
-  // Create a counter variable to keep track of the position of the currently playing text
-  let currentIndex = 0;
-
-  // Function to play the next text in the Map
-  function playNext() {
-    // Get the text at the current position in the Map
-    const text = textMap.get(currentIndex);
-
-    if (text == null || text.length < 3) {
-      console.log("no text, skip..");
-      return;
+      // Increment the counter variable to move to the next position in the Map
+      currentIndex++;
     }
-
-    // If the end of the Map has been reached, return
-    if (currentIndex >= textMap.size) {
-      console.log("already spoke...");
-      return;
-    }
-
-    // Create a new SpeechSynthesisUtterance with the text
-    speakLang(text);
-
-    // Increment the counter variable to move to the next position in the Map
-    currentIndex++;
-
-    console.log(text, currentIndex);
-  }
-
-  // Listen for clicks on the "Listen" button
-  document.querySelector("#listen-button")!.addEventListener("click", () => {
-    // Select all elements that you want to play
-    const elements = document.querySelectorAll(
-      ".bg-gray-50 > .text-base"
-    ) as NodeListOf<HTMLElement>;
-    console.log("find elements =>", elements.length);
-    // Add the text from each element to the Map
-    elements.forEach((element, index) => {
-      if (
-        index >= currentIndex &&
-        element.innerText != null &&
-        element.innerText.length > 3
-      ) {
-        // only add the newer element into the map
-        // only read text inside P elements, to exclude reading code
-        const pElements = element.querySelectorAll(
-          ":scope p"
-        ) as NodeListOf<HTMLElement>;
-        let text = "";
-        for (let i = 0; i < pElements.length; i++) {
-          text += pElements[i].innerText;
-        }
-        console.log("add new text => ", text);
-        textMap.set(index, text);
-      }
-    });
-    playNext();
   });
 
-  // click this button every 5 seconds
-  function clickButton() {
-    // Use the dispatchEvent() method to trigger a click event on the button
-    button.dispatchEvent(new Event("click"));
+  // wait 3 seconds to run again
+  await sleep(3000);
+  await findResponseTextToSpeak();
+}
+
+async function watchParagraphTextsToContinueSpeak(
+  parentNode: HTMLElement,
+  originalParentNodeInnerTextLength: number,
+  originalText: string
+) {
+  // wait 1 secs
+  await sleep(1000);
+
+  const texts = findParagraphTexts(parentNode);
+  const parentNodeInnerTextLength = parentNode.innerText.length;
+  if (texts.length < originalText.length) {
+    throw new Error("odd, the text length became shorter..");
   }
 
-  // Use the setInterval() method to execute the clickButton() function every 5 seconds
-  setInterval(clickButton, 5000);
-});
+  if (
+    texts.length > originalText.length ||
+    parentNodeInnerTextLength > originalParentNodeInnerTextLength
+  ) {
+    // speak the added text
+    const addedText = texts.slice(originalText.length - 1);
+    await speakLang(addedText);
+    // keep watching for texts change
+    await watchParagraphTextsToContinueSpeak(
+      parentNode,
+      parentNodeInnerTextLength,
+      texts
+    );
+  }
+}
+
+function findParagraphTexts(parentNode: HTMLElement): string {
+  const pElements = parentNode.querySelectorAll(
+    ":scope p"
+  ) as NodeListOf<HTMLElement>;
+
+  let text = "";
+  for (let i = 0; i < pElements.length; i++) {
+    text += pElements[i].innerText;
+  }
+  return text;
+}
 
 // utils functions
 // below is all wrote by chatGPT
@@ -140,32 +129,51 @@ function isEnglishOrChinese(texts: string): Language {
   }
 }
 
-function speakLang(texts: string) {
+async function speakLang(texts: string) {
   // Use the isEnglishOrChinese() function to determine the
   // dominant language of the text
   const language = isEnglishOrChinese(texts);
 
   // Create a new SpeechSynthesisUtterance object
   let utterance = new SpeechSynthesisUtterance();
-  const speechSynthesis = window.speechSynthesis;
 
   // Set the text and language of the utterance based on the
   // dominant language of the text
   if (language === Language.Chinese) {
     utterance.text = texts;
     utterance.lang = "zh-CN";
-    // Speak the text using the SpeechSynthesisUtterance API
-    speechSynthesis.speak(utterance);
   } else if (language === Language.English) {
     utterance.text = texts;
     utterance.lang = "en-US";
-    // Speak the text using the SpeechSynthesisUtterance API
-    speechSynthesis.speak(utterance);
   } else {
     // The text is written in a mix of Chinese and English,
     // so we can't determine the dominant language
     console.log(
       "Unable to determine the dominant language of the text, can't speak"
     );
+    return;
   }
+
+  // Speak the text using the SpeechSynthesisUtterance API
+  await speakAndWait(utterance);
 }
+
+// small helper functions
+const speak = (utterance: SpeechSynthesisUtterance) => {
+  const speechSynthesis = window.speechSynthesis;
+  return new Promise((resolve, reject) => {
+    speechSynthesis.speak(utterance);
+    utterance.onend = resolve;
+    utterance.onerror = reject;
+  });
+};
+
+const speakAndWait = async (utterance: SpeechSynthesisUtterance) => {
+  await speak(utterance);
+};
+
+const sleep = (milliseconds: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+};
